5b91643067fd3acdbc6028443420354dc034dff1
WilliamBerlin76 <william.berlin76@gmail.com>
2020-02-20 14:05:31 -0600

    eating cookies comleted from lecture

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,22 +1,42 @@
 #!/usr/bin/python
 
 import sys
-import math
+
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
+# 0: eat 0 ckies 1 time
+# 1: eat 1 cookie 1 time
+# 2: eat 1 cookie 1 time, eat 1 coookie 1 time
+# 2: eat 2 cookies
+# 3: eat 1 cookei 1 time, eat 1 cookie 1 time, eat 1 cookie 1 time
+# 3: eat 2, then 1
+# 3: eat 1, then 2
+# 3: eat 3
+cache = {}
+
 def eating_cookies(n, cache=None):
-  # edge case
-  if n < 1:
+  
+  if n < 0:
+    return 0
+  elif n <= 1:
     return 1
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if cache is None:
+      cache = {}
+    value = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    cache[n] = value
+    return value
+
   # elif n == 2:
   #   return 2
   # what is base case? when will recursion stop
   # find min and max amount of eating methods
   
-  poss_list = []
-  j = 0
-  while sum(poss_list) < n:
+  # while sum(poss_list) < n:
     
   # max_e = n
   
@@ -35,13 +55,10 @@ def eating_cookies(n, cache=None):
   #   min_e = max_e
   
   # print(eating_cookies(n - 1), n, 'recursion')
-  return math.factorial(n) // (math.factorial(4) * math.factorial(n - 4))
-
-print(eating_cookies(10), 'from function')
+  
+# print(eating_cookies(10), 'from function')
  
   
-
-
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
